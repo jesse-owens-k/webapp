5d1df4228a72d8d0dce0046e7145988384adb29f
Jesse Owens K <jesseowens.k@gmail.com>
2018-07-26 22:24:38 +0530

    Update Home.controller.js

--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -5,13 +5,6 @@ sap.ui.define([
 
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
 
-		onDisplayNotFound : function (oEvent) {
-			// display the "notFound" target without changing the hash
-			this.getRouter().getTargets().display("notFound", {
-				fromTarget : "home"
-			});
-		},
-
 		onNavToEmployees : function (oEvent){
 			this.getRouter().navTo("employeeList");
 		},
